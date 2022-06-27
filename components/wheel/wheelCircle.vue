da2c201e585ce1e9984ce4a872835e12e308ced9
bloodsbro <2LzSKmcu2iBxDXMYLdyy6XxEJwNyrtUEYevmEfV7CsGwUpSAsQrKzqkLSt6XEcFx>
2022-06-27 09:56:20 +0300

    upd

--- components/wheel/wheelCircle.vue
+++ components/wheel/wheelCircle.vue
@@ -123,7 +123,7 @@ export default {
       this.isSpinInProgress = false;
       this.reset();
 
-      document.location.replace = item.url;
+      document.location.replace(item.url);
       // this.$modal.show('showWinModal', item);
     },
 
